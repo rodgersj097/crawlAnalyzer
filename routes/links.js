969cf1941a54151a62095a0550b8685e3edae661
rodgersj097 <200320013@student.georgianc.on.ca>
2019-03-23 12:49:09 -0400

    got functinality now need csv to db

--- routes/links.js
+++ routes/links.js
@@ -2,15 +2,64 @@ const express = require('express');
 const router = express.Router(); 
 const db = require('../config/DBConnect')
 const Link = require('../model/link')
-
-
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op; 
+//Get link list
 router.get('/', (req,res) =>
  Link.findAll()
-    .then(links => {
-        console.log(links)
-        res.sendStatus(200);
-    } )
+    .then(links => 
+        res.render('links', {
+            links
+        }) )
     .catch(err => console.log(err))
  )
+
+
+//Add a link 
+router.post('/add', (req, res) =>{
+    const data = { 
+        type: 'HREF',
+        source: "https://stg-www.worldvision.ca/privacy-and-security",
+        destination: "https://my.worldvision.ca/home/index",
+        size: "0",
+        altText:"",
+        anchor: "My World Vision",
+        statusCode:"404",
+        status:"Not Found",
+        follow: "true"
+    }
+
+    let{type, source,destination,size,altText,anchor,statusCode,status,folow} = data
+
+    //Ibsert into tble=[]
+    Link.create({ 
+        type, 
+        source,
+        destination, 
+        size,
+        altText,
+        anchor, 
+        statusCode,
+        status
+      
+       
+    })
+    .then(link => res.redirect('/links'))
+    .catch(err => console.log(err))
+ 
+} )
+
+//search for links 
+router.get('/search', (req,res) => { 
+    let {term} = req.query; 
+    //make lowercase 
+    term = term.toLowerCase(); 
+
+    Link.findAll({ where: {statusCode : {[Op.like] : '%' + term + '%' } } }  )
+    .then(links => res.render('links', {links}))
+    .catch(err => console.log(err))
+
+
+});
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const path = require('path')
 const db = require('./config/DBConnect')
 const fs = require('fs')
 const app = express() 
+//handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+//set public 
+app.use(express.static(path.join(__dirname, 'public' )))
+
+//index route 
+app.get('/', (req,res) => res.render('index', {layout: 'landing'}))
+
 //test DB 
 db.authenticate()
     .then(() => console.log('Database connected .. '))
@@ -16,5 +25,6 @@ app.get('/', (req,res)  => res.send('INDEX'))
 
 //Link Routes
 app.use('/links', require('./routes/links'))
+
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
